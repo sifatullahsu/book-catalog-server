e7d1bd07bed744feaa5cc6cea40b35d82d6838f7
sifatullahsu <personal.sifat@gmail.com>
2023-09-02 22:50:19 +0600

    server setup

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({ path: path.join(process.cwd(), '.env') })
+
+const config = {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  database: process.env.DB_URL
+}
+
+export default config

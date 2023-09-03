0a6978145e2246f93edaf1c6163668c254c4b644
sifatullahsu <personal.sifat@gmail.com>
2023-09-03 22:48:09 +0600

    validate role for categories

--- src/app/modules/category/category.route.ts
+++ src/app/modules/category/category.route.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
+import { validateRole } from '../../middlewares/validateRole'
 import { CategoryController as controller } from './category.controller'
 
 const router = Router()
 
-router.post('/create-category', controller.createData)
+router.post('/create-category', validateRole(['admin']), controller.createData)
 router.get('/', controller.getAllData)
 router.get('/:id', controller.getData)
-router.patch('/:id', controller.updateData)
-router.delete('/:id', controller.deleteData)
+router.patch('/:id', validateRole(['admin']), controller.updateData)
+router.delete('/:id', validateRole(['admin']), controller.deleteData)
 
 export const CategoryRouter = router

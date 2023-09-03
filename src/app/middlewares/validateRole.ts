ae74206bcea56f39fcb8095714ec48e199fda911
sifatullahsu <personal.sifat@gmail.com>
2023-09-03 22:37:17 +0600

    validateRole middleware

---
+++ src/app/middlewares/validateRole.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express'
+import httpStatus from 'http-status'
+import config from '../../config'
+import ApiError from '../../errors/apiError'
+import { verifyToken } from '../../helpers/jwtHelper'
+
+type iRoleType = 'admin' | 'customer'
+
+export const validateRole = (roles: iRoleType[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization
+      if (!token) throw new ApiError(httpStatus.UNAUTHORIZED, 'Unauthorized access')
+
+      const verifyUser = verifyToken(token, config.jwtSecret as string)
+      if (!roles.includes(verifyUser.role)) throw new ApiError(httpStatus.FORBIDDEN, 'Forbidden access')
+
+      req.user = verifyUser
+
+      next()
+    } catch (error) {
+      next(error)
+    }
+  }
+}
